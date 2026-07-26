d3322805236479f73db6dafc296658c9f05e35c5
goldfish <81166150+HUANGgame@users.noreply.github.com>
2026-07-26 18:31:32 +0800

    Add Happy C++ program

---
+++ Happy.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "祝你生日快樂！" << endl;
+    cout << "祝你每天都開心，心想事成！" << endl;
+
+    return 0;
+}
+
+/*
+執行結果：
+
+祝你生日快樂！
+祝你每天都開心，心想事成！
+*/
