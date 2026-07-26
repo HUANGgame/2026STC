8670f1fe3c9fe9629538d22f31ebab8c09492916
goldfish <81166150+HUANGgame@users.noreply.github.com>
2026-07-26 18:46:52 +0800

    Configure C++ build and run

--- Happy.cpp
+++ Happy.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
     cout << "祝你生日快樂！" << endl;
-    cout << "祝你每天都開心，心想事成！" << endl;
+    cout << "祝你在淡江每天都開心，心想事成！" << endl;
 
     return 0;
 }
@@ -12,5 +12,5 @@ int main() {
 執行結果：
 
 祝你生日快樂！
-祝你每天都開心，心想事成！
+祝你在淡江每天都開心，心想事成！
 */
